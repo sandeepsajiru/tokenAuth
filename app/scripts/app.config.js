2d6263d123fcc46d8841190733f30daaafa0630c
Sandeep Wadhwa <sandeep.sajiru@gmail.com>
2015-08-18 15:39:03 +0530

    Added Register View

---
+++ app/scripts/app.config.js
@@ -0,0 +1,13 @@
+angular.module('tokenAuthApp').config(function($stateProvider) {
+    
+  $stateProvider
+    .state('main', {
+      url:'/',
+      templateUrl:'/views/main.html'
+  })
+      
+      .state('register', {
+      url:'/register',
+      templateUrl: 'views/register.html'
+  });
+})
